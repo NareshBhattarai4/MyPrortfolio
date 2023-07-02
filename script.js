85351a564fd4a7f8d453652bd8b9934a54b4e457
NareshBhattarai4 <technologyandapplication@gmail.com>
2023-07-02 15:08:04 +0545

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const icon = document.getElementById("icon");
 const ul = document.getElementById("ul");
+const a=document.querySelectorAll("a");
+const actives=document.querySelector("active");
+
+const ficon=document.getElementById("footer-icon");
 let open = true;
 
+
+ficon.addEventListener("click",()=>{
+  
+window.screenY=1000;
+   
+})
+
+a.forEach((v)=>{
+    v.onclick=()=>{
+        v.classList.add("active");
+        v.classList.remove("active");
+        ul.classList.remove("show");
+        icon.classList.replace("fa-close", "fa-bars");
+       
+    }
+})
 
 function myFun() {
     ul.classList.add("show");
